4df59acaacfcabe93ecc0e4dc099ce8acb630e89
Olivier Cote <olivier.cote2@hotmail.ca>
2024-02-19 22:30:27 -0500

    Final commit, Project is done and ready to send

--- src/Main.java
+++ src/Main.java
@@ -43,16 +43,5 @@ public class Main {
         directeur.setBuilder(canardBleuBuilder);
         Canard cBleu2 = directeur.creerCanard();
 
-        /*
-        // On affiche dans la console que les modèles sont tous des objets différents
-        // grâce au clonage.
-        System.out.println("Adresse différente pour chaque modèle:\n"
-                            + "cRouge1 " + cRouge1+ "\n"
-                            + "cRouge2 " + cRouge2+ "\n"
-                            + "cRouge3 " + cRouge3+ "\n"
-                            + "cBleu1 " + cBleu1 + "\n"
-                            + "cBleu2 " + cBleu2 + "\n" );
-        */
-
     }
 }
